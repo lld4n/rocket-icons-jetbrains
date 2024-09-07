09805f4c9caa852817a4fab261a0e199fc94481b
lldan <safinildan@gmail.com>
2024-09-07 17:54:26 +0300

    fix icons folder

--- prepare/src/start.ts
+++ prepare/src/start.ts
@@ -1,6 +1,9 @@
 import fs = require("fs");
 
 export function start() {
-  fs.rmSync("./src/main/resources/icons", { recursive: true });
-  fs.mkdirSync("./src/main/resources/icons");
+  try {
+    fs.rmSync("./src/main/resources/icons", { recursive: true });
+    fs.mkdirSync("./src/main/resources/icons");
+    fs.writeFileSync("./src/main/resources/icons/.gitkeep", "");
+  } catch (e) {}
 }
